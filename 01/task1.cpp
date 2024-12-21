edaae02c696ea0ba6f56217f5de8e89484d4af50
Dr.Hippo <epicPythonista@hotmail.com>
2024-12-21 02:37:34 +0000

    (01-1) Implement pair distance summing algorithm

--- 01/task1.cpp
+++ 01/task1.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    cout << "Hello World!\n";
+    // Initialize variables
+    ifstream inputFile;
+    int left[1000];
+    int right[1000];
+    string line;
+    string left_str;
+    string right_str;
+    int lineNumber = 0;
+    int pairDistance = 0;
+    int distanceSum = 0;
+
+    inputFile.open("input.txt");
+
+    // Read file contents; parse each line and put numbers into two arrays
+    while (getline(inputFile, line)) {
+        left_str = line.substr(0, 5);
+        right_str = line.substr(8, 5);
+
+        left[lineNumber] = stoi(left_str);
+        right[lineNumber] = stoi(right_str);
+
+        lineNumber++;
+    }
+
+    // Sort arrays
+    sort(left, left + sizeof(left)/sizeof(left[0]));
+    sort(right, right + sizeof(right)/sizeof(right[0]));
+
+    // Loop through arrays; calculate distance between values in each pair and sum them
+    for (int i = 0; i < 1000; i++) {
+        pairDistance = abs(left[i] - right[i]);
+        distanceSum += pairDistance;
+    }
+
+    cout << distanceSum;
+
+    inputFile.close();
     return 0;
 }
