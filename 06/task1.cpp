233d895463e4b4f1d14629575fa8e092ce3c3224
Dr.Hippo <epicPythonista@hotmail.com>
2025-08-06 10:07:52 +0800

    (06-1) Find guard coordinates

--- 06/task1.cpp
+++ 06/task1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <set>
 #include "shared.h"
 using namespace std;
 
@@ -9,19 +10,22 @@ int main() {
     ifstream inputFile;
     string line;
     vector<string> grid;
-    char freeSpace = '.';
+    char obstruction = '#';
     char guard = '^';
+    int lineIndex = 0;
+    pair<int, int> guardPos = {0, 0};
 
     inputFile.open("input.txt");
 
     while (getline(inputFile, line)) {
+        size_t guardXVal = line.find(guard);
+        if (guardXVal != string::npos) guardPos = {guardXVal, lineIndex};
         grid.push_back(line);
+        lineIndex++;
     }
-
-    for(const auto& s: grid) cout << s << endl;
     
     // Display result
-    cout << "Hello World! " << xkcdRand() << endl;
+    cout << "Hello World! " << guardPos.first << " " << guardPos.second << endl;
 
     inputFile.close();
     return 0;
